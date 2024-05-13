24b143dea3900a4b4e77d31b66eee6262c6b52d6
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-05-13 16:57:34 +0600

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const { handlers :{GET ,POST}, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
 });
